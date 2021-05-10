33c77077e983fe05f064c2cce33e8187d1eee5d3
Marwan <marwan@Marwans-MacBook-Pro.local>
2021-05-10 15:28:22 +1000

    Added logic to update DB with confirmed/unconfirmed students, also added method to search db by unique email

--- src/main/java/com/hmses/demo/repository/StudentTutorRepository.java
+++ src/main/java/com/hmses/demo/repository/StudentTutorRepository.java
@@ -3,6 +3,9 @@ package com.hmses.demo.repository;
 import com.hmses.demo.domain.StudentTutor;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface StudentTutorRepository extends CrudRepository<StudentTutor, Integer> {
 
+    List<StudentTutor> findByEmail(String email);
 }
